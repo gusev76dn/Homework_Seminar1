6351005c88d17fd440ddc9abcc52ee94be337b4e
gusev76 <gusev76dn@mail.ru>
2022-10-02 14:26:55 +0300

    добавили папку WORK1

---
+++ WORK1/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача 1
+
+Console.WriteLine("Введите число 1: ");
+string number1 = Console.ReadLine();
+int x1 = int.Parse(number1);
+Console.WriteLine("Введите чмсло 2: ");
+string number2 = Console.ReadLine();
+int x2 = int.Parse(number2);
+
+if (x1 > x2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
