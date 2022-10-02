3513a93ca18c7466d47a5dde363e550d3a6c97ef
gusev76 <gusev76dn@mail.ru>
2022-10-02 15:32:31 +0300

    добавили папку WORK3

---
+++ WORK3/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 3
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int x = int.Parse(number);
+
+if (x % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
