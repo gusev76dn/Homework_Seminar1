4c288615b8643086b2d63a2f3a5708b7ce788c0f
gusev76 <gusev76dn@mail.ru>
2022-10-02 16:18:03 +0300

    добавили папку WORK4

---
+++ WORK4/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 4
+
+Console.WriteLine("Введите число 1: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int y = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= y; i += 1)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
